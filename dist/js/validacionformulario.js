281837b6cdd1213954c8d36387454c1c26b92eaa
Andrud Acosta <andrudacosta@gmail.com>
2022-11-19 22:13:29 -0500

    Trabajo 70%

--- dist/js/validacionformulario.js
+++ dist/js/validacionformulario.js
@@ -1,5 +1,7 @@
 function validacion(){
     if(document.form.nombres.value=="" || document.form.apellidos.value==""  || document.form.documento.value=="" || document.form.telefono.value=="" || document.form.direccion.value=="" || document.form.correo.value=="" || document.form.password.value==""){
         alert("Ingrese todos los campos")
+    }else{
+        window.location="./indexlogueado.html"
     }
 }
